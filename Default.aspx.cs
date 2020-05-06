0b08bc62186b1ab7c0e2feb9dda0a74d9fd32eeb
bachira <bachira2205@gmail.com>
2020-05-06 20:16:02 +0400

    NewCommit

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,31 @@ namespace AssignmentNew
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Testing db connection
+            string connString = "Data Source=DESKTOP-C2S2FND\\SQLEXPRESS;Initial Catalog=UserDB;Integrated Security=SSPI;Pooling=False";
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            SqlCommand command;
+            SqlDataReader dataReader;
 
+            string sql = "Select * from Users";
+
+            command = new SqlCommand(sql, conn);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                if (dataReader.GetValue(0).ToString() == "1")
+                {
+                    break;
+                }
+
+            }
+            dataReader.Close();
+            command.Dispose();
+            conn.Close();
         }
+
     }
 }

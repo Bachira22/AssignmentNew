555c22755b78da77f7e1f63a200abe5c2c99a67c
bachira <bachira2205@gmail.com>
2020-05-12 21:40:54 +0400

    SQL

---
+++ Product.sql
@@ -0,0 +1,14 @@
+﻿create table Products(
+ProductsId int identity(1,1),
+ProductName varchar (max),
+ProductDescription  varchar (max),
+ProductPrice int,
+ProductImage  varchar (max)
+)
+insert into Products(ProductName, ProductDescription, ProductPrice, ProductImage)
+values ('Paracetamol', 'Pain Killer', 2000,'iis.png')
+
+
+
+
+select * from Products
